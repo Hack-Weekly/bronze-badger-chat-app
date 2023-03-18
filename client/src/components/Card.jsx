71aef759d95a93ec383e600228fc727fef32ab21
Marius <marius.horghidan@yahoo.com>
2023-03-19 00:01:25 +0200

    added an alt attribute plus width and height properties to the logo image

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,7 +6,7 @@ export const Card = ({ content }) => {
   return (
     <div className='card md:flex gap-10 w-full max-w-sm md:max-w-2xl m-4 p-4 md:p-10 bg-white shadow-xl rounded-md'>
       <div className='card-left flex flex-col justify-center items-center md:w-1/2 py-10'>
-        <img src={logo}></img>
+        <img src={logo} alt='bronze badger chat logo' width={200} height={131}></img>
         {content}
       </div>
       <div className='card-right md:flex w-1/2 justify-around items-center w-35 relative overflow-hidden i hidden'></div>
