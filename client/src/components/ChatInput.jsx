03d3f64f3bb0debd11d3f294215464bee7225108
lukavelic <96191586+lukavelic@users.noreply.github.com>
2023-03-22 11:18:05 +0100

    Create ChatInput.jsx

---
+++ client/src/components/ChatInput.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import SendIcon from '@mui/icons-material/Send';
+
+export const ChatInput = (props) => {
+  const [text, setText] = useState('');
+  const inputRef = useRef(null);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    props.handleSubmit(e, text);
+    setText('');
+    inputRef.current.value = '';
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className='flex justify-between items-center border-2 h-14 py-2 px-3 rounded-xl mb-4 hover:border-indigo-400 transition ease-in-out bg-white '
+    >
+      <input
+        ref={inputRef}
+        onChange={handleChange}
+        className='w-full pl-2 h-full outline-none border-none'
+      ></input>
+      <button type='submit'>
+        <SendIcon className='text-indigo-400 min-h-[2rem] min-w-[2rem]' />
+      </button>
+    </form>
+  );
+};
+
+ChatInput.propTypes = {
+  handleSubmit: PropTypes.func,
+};
