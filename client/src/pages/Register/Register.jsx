c9208587df8fed43bdc15b7be228f2a1dfa033a1
darko5r <28720396+darko5r@users.noreply.github.com>
2023-03-23 21:21:03 +0100

    Update Register.jsx

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Card } from 'components';
 import { inputsData, registerFormProps } from 'data';
 import axios from 'axios';
@@ -6,14 +6,33 @@ import { useNavigate } from 'react-router-dom';
 import { useForm } from 'hooks/useForm';
 
 export const Register = () => {
+  const [initialValues, setInitialValues] = useState({});
   const { values, onChange } = useForm();
   const navigate = useNavigate();
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/users')
+      .then(response => {
+        setInitialValues(response.data);
+      })
+      .catch((err) => alert(err));
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const changedFields = {};
+    for (const [key, value] of Object.entries(values)) {
+      if (value !== initialValues[key]) {
+        changedFields[key] = value;
+      }
+    }
     axios
-      .post('http://localhost:8080/users', values)
-      .then(() => navigate('/login'))
+      .patch('http://localhost:8080/users', changedFields)
+      .then(response => {
+        setInitialValues(response.data);
+        navigate('/login');
+      })
       .catch((err) => alert(err));
   };
 
