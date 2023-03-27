e5f99c127d9f2731dda4c3157946612b6b185f1a
darko5r <28720396+darko5r@users.noreply.github.com>
2023-03-27 22:38:15 +0200

    Update credentials.ts

--- backend/src/auth/domain/dto/credentials.ts
+++ backend/src/auth/domain/dto/credentials.ts
@@ -1,12 +1,13 @@
-import {ApiProperty} from "@nestjs/swagger";
-import {IsEmail, IsString} from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class Credentials {
     @ApiProperty()
     @IsEmail()
-    email: string
+    email: string;
 
     @ApiProperty()
     @IsString()
-    password: string
+    @MinLength(5)
+    password: string;
 }
