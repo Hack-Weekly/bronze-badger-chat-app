58957f7711e89d0a2c2143eeac088213dc184ac3
hobi9 <hans25@live.it>
2023-03-28 22:13:51 +0200

    secured routes on the frontend

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -5,6 +5,7 @@ import { Form, Card } from 'components';
 import { inputsData, loginFormProps } from 'data';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useSignIn } from 'react-auth-kit';
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:8080',
@@ -12,24 +13,33 @@ const apiClient = axios.create({
 
 export const Login = () => {
   const { values, onChange } = useForm();
+  const signIn = useSignIn();
   const navigate = useNavigate();
 
-  const handleSubmit = useCallback(async (e) => {
-    e.preventDefault();
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
 
-    try {
-      const { access_token } = await apiClient.post('/login', values);
-      localStorage.setItem('token', access_token);
-      navigate('/chat');
-    } catch (err) {
-      alert(err);
-    }
-  }, [navigate, values]);
-
-  const filteredInputsData = useMemo(
-    () => inputsData.filter(({ login }) => login),
-    []
+      try {
+        const { access_token } = await apiClient.post('/login', values);
+        if (
+          signIn({
+            token: access_token,
+            expiresIn: 1440,
+            tokenType: 'Bearer',
+            authState: { email: values.email },
+          })
+        )
+          navigate('/chat');
+        else throw new Error('Error during log-in');
+      } catch (err) {
+        alert(err);
+      }
+    },
+    [navigate, values],
   );
+
+  const filteredInputsData = useMemo(() => inputsData.filter(({ login }) => login), []);
 
   return (
     <div className='flex min-h-screen justify-center items-center'>
