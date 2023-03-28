a7162d8f5b3371c8ffa7f35d17caf932b6fb2205
darko5r <28720396+darko5r@users.noreply.github.com>
2023-03-28 15:24:57 +0200

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,46 @@
-import {Module} from '@nestjs/common';
-import {MongooseModule} from '@nestjs/mongoose';
-import {ConfigModule, ConfigService} from '@nestjs/config';
-import {AuthModule} from './auth/auth.module';
-import {ApiModule} from './api/api.module';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { ApiModule } from './api/api.module';
+import { IoAdapter } from '@nestjs/platform-socket.io'; // needs a re-check added socket.io
+import { IoModule } from '@nestjs/platform-socket.io';
+import * as socketio from 'socket.io';
+import { AppGateway } from './app.gateway'; // needs a re-check added app.gateway
+
+export class CustomSocketAdapter extends IoAdapter {
+  createIOServer(port: number, options?: ServerOptions): Server {
+    const server = super.createIOServer(port, options);
+    const io = server.of('/api/socket');
+    io.origins('*:*');
+    return server;
+  }
+}   // needs a re-check
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            envFilePath: [`env/.${process.env.NODE_ENV}.env`],
-        }),
-        MongooseModule.forRootAsync({
-            useFactory: async (configService: ConfigService) => ({
-                uri: configService.get('DATABASE_URL'),
-            }),
-            inject: [ConfigService],
-        }),
-        AuthModule,
-        ApiModule,
-    ],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`env/.${process.env.NODE_ENV}.env`],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    ApiModule,
+    IoModule.forRoot({
+      adapter: new CustomSocketAdapter(),
+    }),
+  ],
+  providers: [AppGateway],
 })
-export class AppModule {}
+export class AppModule {   // needs a re-check
+  configure(consumer: MiddlewareConsumer) {
+  consumer
+    .apply(MyMiddleware)
+    .forRoutes({ path: '*', method: RequestMethod.ALL });  
+  }
+}  
